71efeec441c370da43edbf3a7fdf7acac0cd75c1
NguyenThien02 <ngpthien02@gmail.com>
2024-09-17 20:36:09 +0700

    Data Transfer Object and Input Validation

---
+++ src/main/java/com/example/ShopApp/Controllers/CategoryController.java
@@ -0,0 +1,48 @@
+package com.example.ShopApp.Controllers;
+
+import com.example.ShopApp.dtos.CategoryDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/categories")
+
+
+public class CategoryController {
+    //Hiện tất cả các Categories
+    @GetMapping("")//http://localhost:8080/api/v1/categories?page=1&limit=10
+    public ResponseEntity<String> getAllCategories(
+            @RequestParam("page") int page,
+            @RequestParam("limit") int limit
+    ) {
+        return ResponseEntity.ok(String.format("GetAllCategory page %d, limit %d", page,limit));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> insertCategory(@Valid @RequestBody CategoryDTO categoryDTO,
+                                                 BindingResult result) {
+        if(result.hasErrors()){
+            List<String> errorMessages = result.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+        return ResponseEntity.ok("This is insert category" + categoryDTO );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable long id) {
+        return ResponseEntity.ok("Insert category with id = " + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable long id) {
+        return ResponseEntity.ok("Delete category with id = " + id);
+    }
+}
