7d1e131d77277333852a094e0dd02af34ec66e61
NguyenThien02 <ngpthien02@gmail.com>
2024-09-20 16:33:32 +0700

    Model Order and OrderDetail

---
+++ src/main/java/com/example/ShopApp/models/OrderDetail.java
@@ -0,0 +1,40 @@
+package com.example.ShopApp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
+
+@Entity
+@Table(name = "order_details")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "price")
+    private Float price;
+
+    @Column(name = "number_of_products",nullable = false, length = 11)
+    private int numberOfProducts;
+
+    @Column(name = "total_money",nullable = false)
+    private Float totalMoney;
+
+    @Column(name = "color", length = 20)
+    private String color;
+}
